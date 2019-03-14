61f7ea1c3a4bfb52c29e4bead617dcd15823f7fd
Dhiraj Eadara <dhiraj.eadara@gramener.com>
2019-03-14 15:34:58 +0530

    ENH: Add get parameters to url and load json data

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path, include
+from django.urls import path
+from django.conf.urls import url
 from . import views
 
 app_name = 'main'
 
 urlpatterns = [
-     path('', views.homepage, name='homepage')
+    path('', views.homepage, name='homepage'),
+    url(r'^data/$', views.viewCSV)
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+import pandas as pd
+import os
+
+directory = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(directory, 'data')
+
 # Create your views here.
+
 
 def homepage(request):
     return HttpResponse("Hello World!!!")
 
+
+def viewCSV(request):
+    if 'filename' in request.GET and request.GET['filename']:
+        filename = request.GET['filename']
+        df = pd.read_csv(os.path.join(DATA_DIR, filename))
+        return HttpResponse(
+            df.to_json(orient='records'), content_type="application/json")
+    else:
+        return HttpResponse('Please submit a search term.')
